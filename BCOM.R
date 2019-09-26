74d08521d54dfdbc0b477d84f543a8303a0398c8
Jason Rodnick <jrodnick@opcm.local>
2019-09-26 14:22:32 -0700

    added lables names, removed density plot

--- BCOM.R
+++ BCOM.R
@@ -11,7 +11,6 @@ BCOM6$Year <- as.factor(BCOM6$Year)
 g <- ggplot(BCOM6, aes(x = X., y = BCOM, color = Year, shape = Year))
 g + geom_point() + theme_light() +
   geom_hline(yintercept = 0) + geom_vline(xintercept = 0) +
-  geom_smooth(method = lm) + facet_grid(~ Year)
+  geom_smooth(method = lm) + facet_grid(~ Year) +
+  xlab("Dollar % Change") + ylab("BCOM % Change")
 
-d <- ggplot(BCOM, aes(x = X., y = BCOM))
-d + geom_density_2d(aes(x = X., y = BCOM, color = Year), stat = 'identity') + geom_point()
