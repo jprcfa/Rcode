354e86b85732d5552bbc24c19dd3fa1b6e81d95d
Jason Rodnick <jrodnick@opcm.local>
2020-02-26 15:25:11 -0800

    updated to ggplot2 for plot engine

--- Drawdown.R
+++ Drawdown.R
@@ -4,7 +4,7 @@ library(xts)
 library(ggplot2)
 library(reshape2)
 
-prices <- read.csv("O:/Administration/Employee Folders/Jason's Folder/R Data/CRM.csv")
+prices <- read.csv("O:/Administration/Employee Folders/Jason's Folder/R Data/SP500.csv")
 date <- as.Date(prices$Date, format = "%m/%d/%Y")
 prices$Date <- date
 prices.xts <- xts(prices[,-1], order.by = prices[,1])
@@ -12,12 +12,15 @@ prices.xts <- xts(prices[,-1], order.by = prices[,1])
 returns.xts <- Return.calculate(prices.xts, method = c("discrete"))
 returns.xts <- returns.xts[-1,]
 
-chart.Drawdown(returns.xts["2017::",], legend.loc = "bottomleft")
-table.Drawdowns(returns.xts["2018::",2])
+chart.Drawdown(returns.xts["2010::",], legend.loc = "bottomleft", plot.engine = "ggplot2")
+table.Drawdowns(returns.xts["2010::",1], top = 10)
+
+?chart.Drawdown
 
 SharpeRatio.annualized(returns.xts["2016::",])
 charts.PerformanceSummary(returns.xts)
-charts.PerformanceSummary(returns.xts["2018::",])
+charts.PerformanceSummary(returns.xts["2010::",])
+
 
 Return.annualized(returns.xts)
 StdDev.annualized(returns.xts)
