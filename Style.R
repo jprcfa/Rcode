25dc6b2670551306412b3592b8bc0c2968fd125d
Jason Rodnick <jrodnick@opcm.local>
2020-07-16 12:34:22 -0700

    added xlc and xlre to analysis

--- Style.R
+++ Style.R
@@ -7,7 +7,6 @@ library(ggplot2)
 library(reshape2)
 
 style <- read.csv("O:/Administration/Employee Folders/Jason's Folder/R Data/Style.csv", stringsAsFactors=FALSE)
-style$OPCM.DE <- as.numeric(style$OPCM.DE)
 date <- as.Date(style$Date, format = "%m/%d/%Y")
 style$Date <- date
 style.xts <- xts(style[,-1], order.by = style[,1])
@@ -15,8 +14,15 @@ style.xts <- xts(style[,-1], order.by = style[,1])
 chart.Drawdown(style.xts["2010::",], legend.loc = "bottomleft", plot.engine = "ggplot2")
 table.Drawdowns(style.xts["2000::",1], top = 10)
 
-chart.RollingStyle(style.xts["2005::",16],style.xts[,1:4], method="constrained", width=18)
-chart.RollingStyle(style.xts["2005::",5],style.xts[,1:4], method="constrained", width=18, cex.legend = .7, colorset=rainbow12equal, las=1)
+chart.RollingStyle(style.xts["2005::",17],style.xts[,1:4], method="constrained", width=24, cex.legend = .7, colorset=tol12qualitative, las=1)
+chart.RollingStyle(style.xts["2005::",17],style.xts[,c(6:11,13:15,5,12)], method="constrained", width=24, cex.legend = .7, colorset=tol12qualitative, las=1)
+chart.RollingStyle(style.xts["2005::",17],style.xts[,c(5:15)], method="constrained", width=24, cex.legend = .7, colorset=tol12qualitative, las=1)
+
+chart.RollingStyle(style.xts["2005::",16],style.xts[,1:4], method="constrained", width=24, cex.legend = .7, colorset=tol12qualitative, las=1)
+chart.RollingStyle(style.xts["2005::",16],style.xts[,c(6:11,13:15,5,12)], method="constrained", width=18, cex.legend = .7, colorset=tol12qualitative, las=1)
+chart.RollingStyle(style.xts["2005::",16],style.xts[,c(5:15)], method="constrained", width=24, cex.legend = .7, colorset=tol12qualitative, las=1)
+
+
 chart.RollingStyle(style.xts[,5],style.xts[,1:4], method="normalized", width=18, cex.legend = .7, colorset=rainbow12equal, las=1)
 
 
