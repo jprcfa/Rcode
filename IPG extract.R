a35906b72acc7cf9d394b2ed91845c6cdd9d630f
Jason Rodnick <jrodnick@opcm.local>
2020-10-16 09:22:34 -0700

    update 10/16/20

--- IPG extract.R
+++ IPG extract.R
@@ -39,6 +39,8 @@ for (i in 1:length(files2))
   text <- tesseract::ocr(files2[i])
 
 files1 <- list.files(path="O:/Administration/Employee Folders/Jason's Folder/R Data/IPG", pattern="*.pdf", full.names=TRUE, recursive=FALSE)
+files1length <- len
+t <- data.frame()
 
 for (i in 1:length(files1)) {
   t[i,1] <- files1[i]
@@ -47,20 +49,22 @@ for (i in 1:length(files1)) {
   cat(text)
   text2 <- strsplit(text, "\n") %>% unlist()
   allocation <- which(grepl("ASSET ALLOCATION GUIDELINES", text2, ignore.case = T))
+  allocation <- allocation - 5
   FI <- which(grepl("Fixed Income", text2, ignore.case = T))
-  if (length(FI) > 1) 
-    FI2 <- FI[length(FI)]
-  else
-    FI2 <-FI
-  allocationlines <- c(allocation:FI2+1)
+  if (length(FI) > 1) {
+    FI2 <- FI[length(FI)] +5
+  } else {
+    FI2 <-FI +5
+  }
+  allocationlines <- c(allocation:FI2)
   allocation_extracted <- text2[allocationlines]
-  t[i,2] <- allocation_extracted[which(grepl("Domestic", allocation_extracted, ignore.case = TRUE))]
-  t[i,3] <- allocation_extracted[which(grepl("Foreign", allocation_extracted, ignore.case = TRUE))]
-  t[i,4] <- allocation_extracted[which(grepl("Natural Resources", allocation_extracted, ignore.case = TRUE))]
-  t[i,5] <- allocation_extracted[which(grepl("REal Estate", allocation_extracted, ignore.case = TRUE))]
-  t[i,6] <- allocation_extracted[which(grepl("Alternative Investments", allocation_extracted, ignore.case = TRUE))]
-  t[i,7] <- allocation_extracted[which(grepl("Fixed Income", allocation_extracted, ignore.case = TRUE))]
-  t[i,8] <- allocation_extracted[which(grepl("Cash", allocation_extracted, ignore.case = TRUE))]
+  t[i,2] <- cat(allocation_extracted[which(grepl("Domestic", allocation_extracted, ignore.case = TRUE))])
+  t[i,3] <- cat(allocation_extracted[which(grepl("Foreign", allocation_extracted, ignore.case = TRUE))])
+  t[i,4] <- cat(allocation_extracted[which(grepl("Natural Resources", allocation_extracted, ignore.case = TRUE))])
+  t[i,5] <- cat(allocation_extracted[which(grepl("REal Estate", allocation_extracted, ignore.case = TRUE))])
+  t[i,6] <- cat(allocation_extracted[which(grepl("Alternative Investments", allocation_extracted, ignore.case = TRUE))])
+  t[i,7] <- cat(allocation_extracted[which(grepl("Fixed Income", allocation_extracted, ignore.case = TRUE))])
+  t[i,8] <- cat(allocation_extracted[which(grepl("Cash", allocation_extracted, ignore.case = TRUE))])
 }
 
 
@@ -71,7 +75,7 @@ test<-cat(t)
 ##########
 eng <- tesseract("eng")
 
-pngfile <- pdftools::pdf_convert("O:/Administration/Employee Folders/Jason's Folder/R Data/IPG/20190617 Lindskog Foundation IPG Signed and Executed.pdf", dpi = 600)
+pngfile <- pdftools::pdf_convert("O:/Administration/Employee Folders/Jason's Folder/R Data/IPG/20170314 Abbe Arner - IPG Signed and Executed.pdf", dpi = 600)
 text <- tesseract::ocr(pngfile)
 cat(text)
 text
@@ -81,9 +85,14 @@ length(FI)
 text2 <- strsplit(text, "\n") %>% unlist()
 allocation <- which(grepl("ASSET ALLOCATION GUIDELINES", text2, ignore.case = T))
 FI <- which(grepl("Fixed Income", text2, ignore.case = T))
-FI2 <- FI[2]  ##need to fix this so only works
+
+if (length(FI) > 1) {
+  FI2 <- FI[length(FI)]
+} else {
+  FI2 <-FI
+}
 #cash <- which(grepl("Cash", text2, ignore.case = T))
-allocationlines <- c(allocation:FI2+1)
+allocationlines <- c(allocation:FI2+5)
 allocationlines
 allocation_extracted <- text2[allocationlines]
 allocation_extracted 
@@ -97,7 +106,7 @@ FE <- allocation_extracted[which(grepl("Foreign", allocation_extracted, ignore.c
 NR <- allocation_extracted[which(grepl("Natural Resources", allocation_extracted, ignore.case = TRUE))]
 RE <- allocation_extracted[which(grepl("REal Estate", allocation_extracted, ignore.case = TRUE))]
 AA <- allocation_extracted[which(grepl("Alternative Investments", allocation_extracted, ignore.case = TRUE))]
-FI <- allocation_extracted[which(grepl("Fixed Income", allocation_extracted, ignore.case = TRUE))]
+FIp <- allocation_extracted[which(grepl("Fixed Income", allocation_extracted, ignore.case = TRUE))]
 C <- allocation_extracted[which(grepl("Cash", allocation_extracted, ignore.case = TRUE))]
 
 
